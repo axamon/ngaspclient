1dae14d1942129f9b2439c3e59c7ee6d0d7fb931
Alberto Bregliano <alberto.bregliano@telecomitalia.it>
2019-08-14 10:46:51 +0200

    gestione flags e args

--- main.go
+++ main.go
@@ -45,10 +45,10 @@ type Configuration struct {
 	Token string `json:"token"`
 }
 
-var tgu = flag.String("t", "", "TGU da controllare")
+// var tgu = flag.String("t", "", "TGU da controllare")
 
 // var configfile = flag.String("c", "conf.json", "File di configurazione")
-var vault = flag.String("s", "127.0.0.1:9999", "Server tokenizzatore e porta")
+var vault = flag.String("s", "10.38.105.251:9999", "Server tokenizzatore e porta")
 
 // var configuration Configuration
 
@@ -57,15 +57,25 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	// Crea il flag per la versione app.
 	version := flag.Bool("v", false, "Versione dell'APP")
+
+	// Parsa i flags
 	flag.Parse()
 
+	// Parsa i non flags e recupera la tgu come primo argomento num: 0
+	tguarg := flag.Arg(0)
+
+	// fmt.Println(tguarg)
+
+	// Se il flag version è settato mostra la versione ed esce.
 	if *version {
 		fmt.Printf("Ver%s\n", BuildVersion)
 		os.Exit(0)
 	}
 
-	lenghthTGU := len(*tgu)
+	// verifica lunghezza tgu inserita
+	lenghthTGU := len(tguarg)
 
 	switch {
 	case lenghthTGU == 0:
@@ -73,7 +83,7 @@ func main() {
 		os.Exit(1)
 	case lenghthTGU < 12:
 		for i := lenghthTGU; i < 12; i++ {
-			*tgu = "0" + *tgu
+			tguarg = "0" + tguarg
 		}
 	case lenghthTGU > 12:
 		fmt.Println("TGU troppo lunga massimo 12 numeri. Rilancia con -h per sintassi.")
@@ -81,7 +91,7 @@ func main() {
 	default:
 	}
 
-	fmt.Println(*tgu)
+	// fmt.Println(tguarg) // debug
 
 	// Verifica che esista un token valido
 	err := verificatoken(ctx, *vault)
@@ -90,53 +100,67 @@ func main() {
 		os.Exit(1)
 	}
 
+	// busta2 è il payload da passare all'endpoint con la tgu all'interno.
 	busta2 := `<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">\n    
 <Body>\n        
 <getTimVisionTrapsRequest xmlns="urn:AxessInterface">\n
-<tgu>` + *tgu + `</tgu>\n
+<tgu>` + tguarg + `</tgu>\n
 </getTimVisionTrapsRequest>\n    
 </Body>\n
 </Envelope>`
 
+	// configura il trasporto http
 	transCfg := &http.Transport{
 		// Ignora certificati SSL scaduti.
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 
+	// Crea un client http
 	client := &http.Client{Transport: transCfg}
+
+	// Endpoint da contattare
 	url := "https://ngasp-ag.tim.it/live/nbi_interfaces/soap/document_literal"
 
-	// fmt.Println(url)
 
+	// trasforma il payload in bytes.
 	body := []byte(busta2)
 
-	// soap action
+	// soap action è un header per comunicare quale API interrogare.
 	soapAction := `"urn:getTimVisionTraps"`
 
+	// Crea la richista http
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
 	if err != nil {
 		log.Printf("ERROR Impossibile creare richiesta: %s\n", err.Error())
 	}
+
+	// Imposta header necessari
 	req.Header.Set("Content-Type", `text/xml; charset="UTF-8"`)
+	// header per comunicare in linguaggio soap l'api da interrogare.
 	req.Header.Set("SOAPAction", soapAction)
 
+	// Lancia la richiesta http e recupera la response
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Printf("ERROR Impossibile inviare richiesta http: %s\n", err.Error())
 	}
+	// chiude il resp.Body come da specifica
 	defer resp.Body.Close()
 
+	// legge il responsbody in bytes
 	responsBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Printf("ERROR Impossibile leggere body reqest: %s\n", err.Error())
 	}
 
-	err = ioutil.WriteFile("test.xml", responsBody, 0666)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// salva il tutto su un file di appoggio
+	// err = ioutil.WriteFile("test.xml", responsBody, 0666)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 	// fmt.Println(string(responsBody)) // Debug
 
-	traps.Parse(ctx, responsBody, *tgu)
+	// Avvia elavorazione traps
+	traps.Parse(ctx, responsBody, tguarg)
 
 }
